526c2bb30d8f082226045fdfa24059a388bc5d72
Gabriel Helko Meyer <ghmeyer0@gmail.com>
2019-07-14 19:14:31 -0300

    Completado desafios da aula 4

---
+++ desafios/aula4/desafio2.py
@@ -0,0 +1,7 @@
+print('========= Desafio 2 =========')
+print('Quando você nasceu?')
+dia = input ('Dia = ')
+mes = input ('Mês = ')
+ano = input ('Ano = ')
+
+print('Voce nasceu no dia ', dia, 'de ', mes, 'de ', ano, '. Correto?')
---
+++ desafios/aula4/desafio3.py
@@ -0,0 +1,5 @@
+print('========= Desafio 3 =========')
+valor1 = input('Infome o primeiro valor: ')
+valor2 = input('Infome o segundo valor: ')
+resultado = valor1 + valor2
+print('A soma dos valores é ', resultado)
